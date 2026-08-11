b304b326b6cc5f8347cc4f15db997e0d003cf844
northsplash <matthewrenner@northsplash.com>
2026-08-11 13:40:03 -0400

    Add PaymentForm component with PaymentPlans integration

--- payments/PaymentForm.jsx
+++ payments/PaymentForm.jsx
@@ -1,1 +1,5 @@
+import React from 'react';
+import PaymentPlans from './PaymentPlans';
+export default function PaymentForm({plan,setPlan}){return <div><h3>Payment option</h3><PaymentPlans value={plan} onChange={setPlan}/></div>}
+
 
