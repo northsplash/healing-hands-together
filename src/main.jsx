df5adb42757849766f25ac021e1400d78df1f260
northsplash <matthewrenner@northsplash.com>
2026-08-10 23:33:16 -0400

    Refactor main.jsx to use React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter
+} from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+import App from "./App";
+import "./styles.css";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
